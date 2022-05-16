69248f8513b7c08ae84e0aeba24cd5660935f274
Matthew Davey <matthew.davey@dotbunny.com>
2022-05-15 21:23:36 -0400

    Added intial support for type checking to Serializable Dictionary #GDX-54 Done

--- Projects/GDX_Development/Assets/Tests/001_SerializableDictionary/SerializableDictionary_TypeCheck.cs
+++ Projects/GDX_Development/Assets/Tests/001_SerializableDictionary/SerializableDictionary_TypeCheck.cs
@@ -1,8 +1,8 @@
 #if !UNITY_DOTSRUNTIME
 
+using System.Collections.Generic;
 using GDX.Collections.Generic;
 using GDX.Developer.Reports.NUnit;
-using UnityEngine;
 
 namespace BVT
 {
@@ -11,39 +11,22 @@ namespace BVT
     public class SerializableDictionary_TypeCheck : GDX.Developer.Reports.BuildVerification.SimpleTestBehaviour
 #pragma warning restore IDE1006
     {
-        public struct TestStruct
-        {
-            public bool Hello;
-            public bool Jello;
-        }
+        public SerializableDictionary<UnityEngine.Object, UnityEngine.Object> ObjectToObject = new SerializableDictionary<UnityEngine.Object, UnityEngine.Object>();
 
-        public GameObject CameraObject;
-        public GameObject ColliderObject;
-        public GameObject ContentObject;
-        public GameObject CubeObject;
-        public TestStruct StructOne;
-        public TestStruct StructTwo;
-
-        public SerializableDictionary<TestStruct, int> TestStructToInt = new SerializableDictionary<TestStruct, int>();
-        public SerializableDictionary<int, TestStruct> IntToTestStruct = new SerializableDictionary<int, TestStruct>();
-
-        public SerializableDictionary<int, GameObject> IntegerToGameObject =
-            new SerializableDictionary<int, GameObject>();
-
-        public SerializableDictionary<GameObject, int> GameObjectToInteger =
-            new SerializableDictionary<GameObject, int>();
-
-        public SerializableDictionary<string, GameObject> StringToGameObject =
-            new SerializableDictionary<string, GameObject>();
-
-        public SerializableDictionary<GameObject, string> GameObjectToString =
-            new SerializableDictionary<GameObject, string>();
 
         public override TestCase Check()
         {
-            // TODO: TESTS
+            bool foundUnityObjectNull = false;
+            foreach (KeyValuePair<UnityEngine.Object, UnityEngine.Object> kvp in ObjectToObject)
+            {
+                if (kvp.Key == null || kvp.Value == null)
+                {
+                    foundUnityObjectNull = true;
+                }
+            }
+            
             return GDX.Developer.Reports.BuildVerificationReport.Assert(GetIdentifier(),
-                true, "Expected no null references.");
+                !foundUnityObjectNull, "Expected no null references.");
         }
 
         public override string GetIdentifier()
