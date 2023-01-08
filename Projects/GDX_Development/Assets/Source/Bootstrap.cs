8b5acb2d5526ba9b7f5076c0b22f7e4950c0d607
Matthew Davey <matthew.davey@dotbunny.com>
2023-01-07 21:24:12 -0500

    Revert "Move to coroutine method"
    This reverts commit 41270b5afade8a6c2374fe18e6b91aabd09ef780.

--- Projects/GDX_Development/Assets/Source/Bootstrap.cs
+++ Projects/GDX_Development/Assets/Source/Bootstrap.cs
@@ -1,8 +1,14 @@
-using System.Collections;
+
+using System;
 using System.IO;
+using System.Threading.Tasks;
 using GDX;
 using GDX.Developer.Reports.BuildVerification;
+#if !UNITY_EDITOR
+using GDX.Threading;
+#endif
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Dev
 {
@@ -22,42 +28,47 @@ namespace Dev
 
 #pragma warning disable IDE0051
         // ReSharper disable UnusedMember.Local
-        IEnumerator Start()
+        async void Start()
         {
             int testCount = ClassicBuildScenes.Length;
 #if !UNITY_EDITOR
             Debug.Log($"[BOOTSTRAP] Starting test ({testCount.ToString()}) run  ...");
 #endif
-
-            // Build out scene definitions
-            TestRunner.TestScene[] scenes = new TestRunner.TestScene[testCount-1];
-            for (int testSceneIndex = 1; testSceneIndex < testCount; testSceneIndex++)
+            try
             {
-                scenes[testSceneIndex-1] = new TestRunner.TestScene(testSceneIndex);
+                for (int testSceneIndex = 1; testSceneIndex < testCount; testSceneIndex++)
+                {
+                    await TestRunner.EvaluateTestScene(SceneUtility.GetBuildIndexByScenePath(ClassicBuildScenes[testSceneIndex]));
+                }
             }
-
-            yield return TestRunner.Process(scenes);
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                GDX.Developer.Reports.BuildVerificationReport.Panic($"EXCEPTION: {e.Message}.\n{e.StackTrace}");
+            }
+            finally
+            {
+#if UNITY_EDITOR
+                string outputPath = Path.GetFullPath(Path.Combine(Platform.GetOutputFolder("GDX_Automation"), "BVT.xml"));
+#else
+                string outputPath = Path.GetFullPath(Path.Combine(Platform.GetOutputFolder(), "BVT.xml"));
+#endif
+                string result = GDX.Developer.Reports.BuildVerificationReport.OutputReport(outputPath);
+                if (File.Exists(outputPath))
+                {
+                    Debug.Log($"[BOOTSTRAP] Build checks ({result}) written to {outputPath}.");
+                }
+                else
+                {
+                    Debug.LogError($"[BOOTSTRAP] Unable to write file to {outputPath}.");
+                }
 
 #if UNITY_EDITOR
-            string outputPath = Path.GetFullPath(Path.Combine(Platform.GetOutputFolder("GDX_Automation"), "BVT.xml"));
+                UnityEditor.EditorApplication.isPlaying = false;
 #else
-            string outputPath = Path.GetFullPath(Path.Combine(Platform.GetOutputFolder(), "BVT.xml"));
+                Application.Quit();
 #endif
-            string result = GDX.Developer.Reports.BuildVerificationReport.OutputReport(outputPath);
-            if (File.Exists(outputPath))
-            {
-                Debug.Log($"[BOOTSTRAP] Build checks ({result}) written to {outputPath}.");
             }
-            else
-            {
-                Debug.LogError($"[BOOTSTRAP] Unable to write file to {outputPath}.");
-            }
-
-#if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
-#else
-            Application.Quit();
-#endif
         }
     }
 }
