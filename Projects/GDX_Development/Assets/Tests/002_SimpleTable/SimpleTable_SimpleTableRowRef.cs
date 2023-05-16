e254871887765537510a2297b5a9d2c772e24912
Matthew Davey <matthew.davey@dotbunny.com>
2023-05-16 14:13:53 -0400

    Test content for SimpleTable

---
+++ Projects/GDX_Development/Assets/Tests/002_SimpleTable/SimpleTable_SimpleTableRowRef.cs
@@ -0,0 +1,38 @@
+﻿#if !UNITY_DOTSRUNTIME
+
+using GDX;
+using GDX.Collections.Generic;
+using GDX.Developer.Reports.NUnit;
+using UnityEngine;
+
+namespace BVT
+{
+#pragma warning disable IDE1006
+    // ReSharper disable once InconsistentNaming
+    public class SimpleTable_SimpleTableRowRef : GDX.Developer.Reports.BuildVerification.SimpleTestBehaviour
+#pragma warning restore IDE1006
+    {
+        public SimpleTable.SimpleTableRowRef Reference;
+        
+        public override TestCase Check()
+        {
+            // bool found = GameObjectToGameObject.TryGetValue(KeyObject, out GameObject foundObject);
+            // bool correctName = false;
+            // if (found)
+            // {
+            //     correctName = foundObject.name == "ReferencedName";
+            //
+            // }
+            // return GDX.Developer.Reports.BuildVerificationReport.Assert(GetIdentifier(),
+            //     found && correctName, "Expected no null references.");
+
+            return GDX.Developer.Reports.BuildVerificationReport.Assert(GetIdentifier(), true, null, 0);
+        }
+
+        public override string GetIdentifier()
+        {
+            return "SimpleTable.SimpleTableRowRef";
+        }
+    }
+}
+#endif
