696d93a3a2cdac25f8e7ede11525304a94656e42
Matthew Davey <matthew.davey@dotbunny.com>
2023-05-25 07:43:25 -0400

    Add internal demo asset creator

--- Projects/GDX_Development/Assets/Source/Editor/MenuItems.cs
+++ Projects/GDX_Development/Assets/Source/Editor/MenuItems.cs
@@ -1,3 +1,10 @@
+using System;
+using GDX;
+using GDX.Editor.Inspectors;
+#if UNITY_2022_2_OR_NEWER
+using GDX.Editor.Windows.Tables;
+using GDX.Tables;
+#endif
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
@@ -26,6 +33,84 @@ namespace Dev.Editor
             EditorSceneManager.OpenScene("Assets/GDX.unity", OpenSceneMode.Single);
             EditorApplication.EnterPlaymode();
         }
+
+#if UNITY_2022_2_OR_NEWER
+#if GDX_TOOLS
+        [MenuItem("Tools/GDX/INTERNAL/Create StableTable Example", false)]
+#endif
+        public static void CreateStableTableExample()
+        {
+            StableTable asset = ScriptableObject.CreateInstance<StableTable>();
+            AssetDatabase.CreateAsset(asset, "Assets/StableTableExample.asset");
+            AssetDatabase.SaveAssets();
+
+            EditorUtility.FocusProjectWindow();
+            Selection.activeObject = asset;
+
+            asset.AddColumn(Serializable.SerializableTypes.String, "String");
+            asset.AddRow("0");
+            asset.AddColumn(Serializable.SerializableTypes.Char, "Char");
+            asset.AddRow("1");
+            asset.AddColumn(Serializable.SerializableTypes.Bool, "Bool");
+            asset.AddRow("2");
+            asset.AddColumn(Serializable.SerializableTypes.SByte, "SByte");
+            asset.AddRow("3");
+            asset.AddColumn(Serializable.SerializableTypes.Byte, "Byte");
+            asset.AddRow("4");
+            asset.AddColumn(Serializable.SerializableTypes.Short, "Short");
+            asset.AddRow("5");
+            asset.AddColumn(Serializable.SerializableTypes.UShort, "UShort");
+            asset.AddRow("6");
+            asset.AddColumn(Serializable.SerializableTypes.Int, "Int");
+            asset.AddRow("7");
+            asset.AddColumn(Serializable.SerializableTypes.UInt, "UInt");
+            asset.AddRow("8");
+            asset.AddColumn(Serializable.SerializableTypes.Long, "Long");
+            asset.AddRow("9");
+            asset.AddColumn(Serializable.SerializableTypes.ULong, "ULong");
+            asset.AddRow("10");
+            asset.AddColumn(Serializable.SerializableTypes.Float, "Float");
+            asset.AddRow("11");
+            asset.AddColumn(Serializable.SerializableTypes.Double, "Double");
+            asset.AddRow("12");
+            asset.AddColumn(Serializable.SerializableTypes.Bounds, "Bounds");
+            asset.AddRow("13");
+            asset.AddColumn(Serializable.SerializableTypes.Vector2, "Vector2");
+            asset.AddRow("14");
+            asset.AddColumn(Serializable.SerializableTypes.Vector3, "Vector3");
+            asset.AddRow("15");
+            asset.AddColumn(Serializable.SerializableTypes.Vector4, "Vector4");
+            asset.AddRow("16");
+            asset.AddColumn(Serializable.SerializableTypes.Vector2Int, "Vector2Int");
+            asset.AddRow("17");
+            asset.AddColumn(Serializable.SerializableTypes.Vector3Int, "Vector3Int");
+            asset.AddRow("18");
+            asset.AddColumn(Serializable.SerializableTypes.Quaternion, "Quaternion");
+            asset.AddRow("19");
+            asset.AddColumn(Serializable.SerializableTypes.Rect, "Rect");
+            asset.AddRow("20");
+            asset.AddColumn(Serializable.SerializableTypes.RectInt, "RectInt");
+            asset.AddRow("21");
+            asset.AddColumn(Serializable.SerializableTypes.Color, "Color");
+            asset.AddRow("22");
+            asset.AddColumn(Serializable.SerializableTypes.LayerMask, "LayerMask");
+            asset.AddRow("23");
+            asset.AddColumn(Serializable.SerializableTypes.Bounds, "Bounds");
+            asset.AddRow("24");
+            asset.AddColumn(Serializable.SerializableTypes.BoundsInt, "BoundsInt");
+            asset.AddRow("25");
+            asset.AddColumn(Serializable.SerializableTypes.Hash128, "Hash128");
+            asset.AddRow("26");
+            asset.AddColumn(Serializable.SerializableTypes.Gradient, "Gradient");
+            asset.AddRow("27");
+            asset.AddColumn(Serializable.SerializableTypes.AnimationCurve, "AnimationCurve");
+            asset.AddRow("28");
+            asset.AddColumn(Serializable.SerializableTypes.Object, "Object");
+            asset.AddRow("29");
+
+            TableWindowProvider.OpenAsset(asset);
+        }
+#endif
     }
 }
 
